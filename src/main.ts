8dadba4b8ef06b33948d66ca4a3a758bbcb318f8
behlulvardal <behlulvardal@gmail.com>
2024-02-27 17:42:20 -0500

    install phaser package

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import phaser from 'phaser';
+
+class Game extends phaser.Scene 
+{
+    preload() {}
+    create() {}
+
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    Scene: Game,
+    physics: {
+        default: 'arcade'
+    }
+
+};
+
+const game = new Phaser.Game(config)
